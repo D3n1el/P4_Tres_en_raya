198d637c11447b5c45bca478c055d8c42b23cb69
D3n1el <dani.alcaraz.p@gmail.com>
2024-12-03 16:27:06 +0100

    2.1- Comentarios
    Se han añadido comentarios al código

--- src/Tres_en_raya/Tres_en_raya.java
+++ src/Tres_en_raya/Tres_en_raya.java
@@ -10,22 +10,26 @@ public class Tres_en_raya {
 		// TODO Auto-generated method stub
 		
 		Scanner s = new Scanner(System.in);
-		Random r = new Random();
+		Random r = new Random(); 
 		
-		String[] nombres = {"", ""};
+		
+		String[] nombres = {"", ""}; //Delcaración del array donde se introducen los nombres de los jugadores
 		
 		for (int i = 0; i < 2; i++) {
 			System.out.print("Jugador " + (i + 1) + ", ingresa tu nombre: ");
 				nombres[i] = s.nextLine();
 		}
 		
-		int jugadaInicial = r.nextInt(2) + 1;
+		int jugadaInicial = r.nextInt(2) + 1; //Genera un numero entre 1 y 2 de forma aleatoria
 		
+		//Dependiendo del número generado, el programa informa de que jugador empieza.
 		if (jugadaInicial == 1) {
 			System.out.print("Empieza el jugador " + nombres[0]);
 		} else if (jugadaInicial == 2) {
 			System.out.print("Empieza el jugador " + nombres[1]);
 		}
+		
+		
 		
 	}
 
