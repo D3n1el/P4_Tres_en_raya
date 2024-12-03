847c29ea8f2ba3cfcb091f8dabc570cdfd3e2685
D3n1el <dani.alcaraz.p@gmail.com>
2024-12-03 15:53:29 +0100

    1- Ingresar nombres
    El programa pide a cada jugador que introduzca un nombre. Se declaran variables necesarias para levar a cabo dicho objetivo.

--- src/Tres_en_raya/Tres_en_raya.java
+++ src/Tres_en_raya/Tres_en_raya.java
@@ -1,10 +1,24 @@
 package Tres_en_raya;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Tres_en_raya {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner s = new Scanner(System.in);
+		
+		String[] nombres = {"", ""};
+		
+		for (int i = 0; i < 2; i++) {
+			System.out.print("Jugador " + (i + 1) + ", ingresa tu nombre: ");
+				nombres[i] = s.nextLine();
+		}
+		
+		
+		
 	}
 
 }
