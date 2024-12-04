2f9a1be3dbd82e3106f6018c3d53c915ee4d9712
D3n1el <dani.alcaraz.p@gmail.com>
2024-12-04 14:20:00 +0100

    5- Selección
    Se ha añadido el código que permite a cada jugador seleccionar la fila y la columna donde quiera poner ficha

--- src/Tres_en_raya/Tres_en_raya.java
+++ src/Tres_en_raya/Tres_en_raya.java
@@ -13,7 +13,8 @@ public class Tres_en_raya {
 		Random r = new Random(); 
 		
 		boolean casillasRellenadas = false;
-		
+		int f = 0; //Define la fila seleccionada por el jugador
+		int c = 0; //Define la columna seleccionada por el jugador
 		String[] nombres = {"", ""}; //Delcaración del array donde se introducen los nombres de los jugadores
 		char[][] posiciones = {{'_', '_', '_'},
 							   {'_', '_', '_'},	
@@ -56,6 +57,20 @@ public class Tres_en_raya {
 					System.out.println();
 				}
 				
+				do { //Mientras el jugador ponga un numero inválido, el programa seguirá pidiendo indefinidamente que ponga un numero correcto.
+					System.out.print("¿Que fila quieres seleccionar (Opciones: 1, 2 ,3)? "); //Pide al usuario poner un numero del 1 al 3.
+					f = s.nextInt();
+					if (f != 1 && f != 2 && f != 3) {System.out.println("¡NUMERO INVÁLIDO!");} //En caso de poner un numero que no sea 1, ni 2, ni 3, informa de que el numero no es válido.
+				} while (f != 1 && f != 2 && f != 3); //Si el jugador ha puesto 1, 2 o 3, entyonces el programa termina el bucle, dado que el numero puesto es válido.
+					
+				System.out.println();
+					
+				do {
+					System.out.print("¿Qué columna quieres seleccionar (Opciones: 1, 2, 3)? ");
+					c = s.nextInt();
+					if (c != 1 && c != 2 && c != 3) {System.out.println("¡NUMERO INVÁLIDO!");}
+				} while (c != 1 && c != 2 && c != 3);
+					
 				for (int ii = 0; i < posiciones.length; ii++) {
 					for (int j = 0; j < posiciones.length; j++) {
 						if (posiciones[j][i] == '_') {
